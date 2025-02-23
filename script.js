e5f171a9eae7694bb59061808557b58c7aaeb38e
Brandon Ide <brandon.d.ide@gmail.com>
2025-02-22 21:30:57 -0500

    First Commit

---
+++ script.js
@@ -0,0 +1,27 @@
+// Video muting functionality
+const video = document.getElementById('projects');
+video.muted = true;
+
+// Slideshow Controls
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+};
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+};
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("projectSlides");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex-1].style.display = "block";
+};
